6313de12e7fc7c22ce9b3c6f62688fc12e280d25
Anatolii Klots <Anatolii.Klots@albumprinter.com>
2016-03-31 14:48:44 +0200

    Sample added

---
+++ samples/WebServerDemo/MainPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System.IO;
+using System.Net;
+using System.Linq;
+using System.Text;
+using Griffin.Net.Channels;
+using Griffin.Net.Protocols.Http;
+
+using Windows.Networking.Connectivity;
+
+namespace WebServerDemo
+{
+    public sealed partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            var listener = new HttpListener();
+            listener.MessageReceived = OnMessage;
+            listener.Start(IPAddress.Parse(GetLocalIp()), 8080);
+
+        }
+
+        private static void OnMessage(ITcpChannel channel, object message)
+        {
+            var request = (IHttpRequest)message;
+            var response = request.CreateResponse();
+
+            if (request.Uri.AbsolutePath == "/favicon.ico")
+            {
+                response.StatusCode = 404;
+                channel.Send(response);
+                return;
+            }
+
+            var msg = Encoding.UTF8.GetBytes("Hello world");
+            response.Body = new MemoryStream(msg);
+            response.ContentType = "text/plain";
+            channel.Send(response);
+
+            if (request.HttpVersion == "HTTP/1.0")
+                channel.Close();
+        }
+
+        private static string GetLocalIp()
+        {
+            var icp = NetworkInformation.GetInternetConnectionProfile();
+
+            if (icp?.NetworkAdapter == null) return null;
+            var hostname =
+                NetworkInformation.GetHostNames()
+                    .SingleOrDefault(
+                        hn =>
+                            hn.IPInformation?.NetworkAdapter != null && hn.IPInformation.NetworkAdapter.NetworkAdapterId
+                            == icp.NetworkAdapter.NetworkAdapterId);
+
+            // the ip address
+            return hostname?.CanonicalName;
+        }
+    }
+}
